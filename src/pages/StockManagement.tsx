ed5566ed1f7b3e7325dda0ee05b7733c110c0ce2
Justus <justmurenga@gmail.com>
2025-08-25 09:19:56 +0300

    routing issues unified

--- src/pages/StockManagement.tsx
+++ src/pages/StockManagement.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StockManagement as StockManagementComponent } from '@/components/StockManagement';
-import { TenantAdminLayout } from '@/components/layouts/TenantAdminLayout';
+import { TenantAdminLayout } from '@/components/TenantAdminLayout'; // Fixed import path
 
 export function StockManagement() {
   return (
